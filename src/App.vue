d8c8bb531780972bf78d4a56952483b604a05e57
wmaqingbo <wmaqingbo@163.com>
2017-11-13 21:24:41 +0800

    App.vue

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<template>
+<div id="app">
+  <vheader :seller="seller"></vheader>
+  <div class="tab">
+    <div class="tab-item">
+      <!-- 使用 router-link 组件来导航. -->
+      <!-- 通过传入 `to` 属性指定链接. -->
+      <!-- <router-link> 默认会被渲染成一个 `<a>` 标签 -->
+      <router-link :to="{ name: 'goods'}">商品</router-link>
+    </div>
+    <div class="tab-item">
+      <router-link :to="{ name: 'ratings'}">评论</router-link>
+    </div>
+    <div class="tab-item">
+      <router-link :to="{ name: 'seller'}">商家</router-link>
+    </div>
+  </div>
+  <!-- 路由出口 -->
+  <!-- 路由匹配到的组件将渲染在这里 -->
+  <router-view :seller="seller"></router-view>
+</div>
+</template>
+
+<script>
+import header from './components/header/header'
+
+const ERR_OK = 0
+
+export default {
+  data() {
+    return {
+      seller: {}
+    }
+  },
+  created() {
+    this.$http.get('/api/seller').then((response) => {
+      // console.log(response.body)
+      let res = response.body
+      if (res.errno === ERR_OK) {
+        // console.log(res.data)
+        this.seller = res.data
+      }
+    })
+  },
+  components: {
+    'vheader': header
+  }
+}
+</script>
+
+<style lang="scss">
+@import 'static/sass/mixin.scss';
+
+#app {
+    .tab {
+        display: flex;
+        width: 100%;
+        height: 40px;
+        line-height: 40px;
+        text-align: center;
+        // background-color: pink;
+        @include border-1px(rgba(7,17,27,.1));
+        .tab-item {
+            flex: 1;
+            > a {
+                display: block;
+                width: 100%;
+                height: 100%;
+                font-size: 14px;
+                color: rgb(77,85,93);
+                &.router-link-active {
+                    color: rgb(240,20,20);
+                }
+            }
+        }
+    }
+}
+</style>
